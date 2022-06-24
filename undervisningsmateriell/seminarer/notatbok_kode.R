0376dd2e5171adedb7a34059838a583ff488f48f
Martin Søyland <m.g.soyland@stv.uio.no>
2022-06-24 15:05:41 +0200

    oppdaterte manifest

--- undervisningsmateriell/seminarer/notatbok_kode.R
+++ undervisningsmateriell/seminarer/notatbok_kode.R
@@ -1,6 +1,6 @@
-## ----setup, echo=FALSE, message=FALSE, error=FALSE----------------------------------
+## ----setup, echo=FALSE, message=FALSE, error=FALSE-------------------------------------------------------------------
 
-# set.wd("./undervisningsmateriell/seminarer")
+# setwd("./undervisningsmateriell/seminarer")
 library(knitr)
 library(formatR)
 library(rmdformats)
@@ -14,12 +14,17 @@ knitr::opts_chunk$set(
 
 library(stringr)
 library(dplyr) 
-
+library(ggplot2)
 # if("tidytext" %in% installed.packages() == FALSE) install.packages("tidytext")
 
 
 
-## ----laste_data_data----------------------------------------------------------------
+## ----child = 'oppgavebygging.Rmd'------------------------------------------------------------------------------------
+
+
+
+
+## ----laste_data_data-------------------------------------------------------------------------------------------------
 
 saker <- stortingscrape::cases$root
 
@@ -30,19 +35,19 @@ saker %>%
 
 
 
-## ----rda_save, eval=FALSE-----------------------------------------------------------
+## ----rda_save, eval=FALSE--------------------------------------------------------------------------------------------
 ## 
 ## save(saker, file = "./data/saker.rda")
 ## 
 
 
-## ----rda_load-----------------------------------------------------------------------
+## ----rda_load--------------------------------------------------------------------------------------------------------
 
 load("./data/saker.rda")
 
 
 
-## ----csv----------------------------------------------------------------------------
+## ----csv-------------------------------------------------------------------------------------------------------------
 
 library(readr)
 
@@ -50,7 +55,51 @@ saker <- read_csv("./data/saker.csv", show_col_types = FALSE)
 
 
 
-## ----msw_zip------------------------------------------------------------------------
+## ----sav-------------------------------------------------------------------------------------------------------------
+library(haven)
+saker <- read_sav("./data/saker.sav")
+
+
+
+## ----dta-------------------------------------------------------------------------------------------------------------
+
+saker <- read_dta("./data/saker.dta")
+
+
+
+## ----txt-------------------------------------------------------------------------------------------------------------
+
+filer <- list.files("./data/txt", pattern = ".txt", full.names = TRUE)
+filer
+
+titler <- lapply(filer, readLines)
+class(titler)
+
+# Første tekst
+titler[[1]]
+
+
+
+## ----txt2------------------------------------------------------------------------------------------------------------
+
+names(titler) <- str_extract(filer, "[0-9]+")
+names(titler)
+
+
+
+## ----txt3------------------------------------------------------------------------------------------------------------
+saker_txt <- data.frame(titler = unlist(titler),
+                        id = names(titler))
+
+
+## ----txt4------------------------------------------------------------------------------------------------------------
+saker_merge <- left_join(saker_txt, saker[, c("id", "title")], by = "id")
+
+saker_merge$titler == saker_merge$title
+
+
+
+## ----msw_zip---------------------------------------------------------------------------------------------------------
 
 unzip("data/ba_thesis.docx", exdir = "data/wordfiles")
 
@@ -58,14 +107,14 @@ list.files("data/wordfiles/")
 
 
 
-## ----read_msw_feil, warning=TRUE----------------------------------------------------
+## ----read_msw_feil, warning=TRUE-------------------------------------------------------------------------------------
 
 readLines("./data/ba_thesis.docx", n = 2)
 
 
 
 
-## ----docx---------------------------------------------------------------------------
+## ----docx------------------------------------------------------------------------------------------------------------
 
 library(textreadr)
 
@@ -75,7 +124,7 @@ ba_docx[43:46]
 
 
 
-## ----read_pdf-----------------------------------------------------------------------
+## ----read_pdf--------------------------------------------------------------------------------------------------------
 
 ba_pdf <- read_pdf("./data/ba_thesis.pdf")
 
@@ -87,7 +136,7 @@ ba_pdf[11:14]
 
 
 
-## ----bow, tidy=TRUE, results='markup'-----------------------------------------------
+## ----bow, tidy=TRUE, results='markup'--------------------------------------------------------------------------------
 
 regndans <- readLines("./data/regndans.txt")
 
@@ -99,17 +148,98 @@ cat(bow[sample(1:length(bow))])
 
 
 
-## ----dragepust----------------------------------------------------------------------
+## ----dragepust-------------------------------------------------------------------------------------------------------
 
 regndans[which(str_detect(regndans, "dragepust"))]
 
 
 
-## ----regndans_full, echo=FALSE------------------------------------------------------
+## ----regndans_full, echo=FALSE---------------------------------------------------------------------------------------
 cat(paste(regndans, collapse = "\n"))
 
 
-## ----writeManifest, echo=FALSE, eval=FALSE------------------------------------------
+## ----norsentlex, eval=-1---------------------------------------------------------------------------------------------
+devtools::install_github("martigso/NorSentLex")
+
+library(NorSentLex)
+
+# Ordbøker i fullform
+names(nor_fullform_sent)
+
+# Ordbøker for lemma med PoS-tags
+names(nor_lemma_sent)
+
+
+
+## ----pos_fullform, echo=-1-------------------------------------------------------------------------------------------
+set.seed(58493)
+nor_fullform_sent$positive %>% head()
+nor_fullform_sent$positive %>% tail()
+nor_fullform_sent$positive %>% sample(., 6)
+
+
+
+## ----pos_subst, echo=-1----------------------------------------------------------------------------------------------
+set.seed(8943)
+nor_lemma_sent$lemma_noun_positive %>% sample(., 6)
+
+
+
+## ----regndans_sent_setup---------------------------------------------------------------------------------------------
+
+library(tidytext)
+
+load("./data/no4.rda")
+
+no4 <- no4 %>% 
+  group_by(titler) %>% 
+  unnest_tokens(ord, tekst)
+
+
+
+## ----regndans_sent---------------------------------------------------------------------------------------------------
+
+
+no4$pos_sent <- ifelse(no4$ord %in% nor_fullform_sent$positive, 1, 0)
+no4$neg_sent <- ifelse(no4$ord %in% nor_fullform_sent$negative, 1, 0)
+
+table(no4$pos_sent, 
+      no4$neg_sent, 
+      dnn = c("positiv", "negativ"))
+
+
+
+## ----no4_sent_sum----------------------------------------------------------------------------------------------------
+
+no4_sent <- no4 %>% 
+  group_by(titler) %>% 
+  summarize(pos_sent = mean(pos_sent),
+            neg_sent = mean(neg_sent)) %>% 
+  mutate(sent = pos_sent - neg_sent)
+
+no4_sent
+
+
+## ----no4_vis---------------------------------------------------------------------------------------------------------
+
+no4_sent %>% 
+  mutate(neg_sent = neg_sent * -1) %>% 
+  ggplot(., aes(x = str_c(sprintf("%02d", 1:12),
+                          ". ",
+                          str_sub(titler, 1, 7),
+                          "[...]"))) +
+  geom_point(aes(y = neg_sent, color = "Negativ")) +
+  geom_point(aes(y = pos_sent, color = "Positiv")) +
+  geom_point(aes(y = sent, color = "Snitt")) +
+  geom_linerange(aes(ymin = neg_sent, ymax = pos_sent), color = "gray40") +
+  scale_color_manual(values = c("red", "cyan", "gray70")) +
+  labs(x = NULL, y = "Sentiment", color = NULL) +
+  ggdark::dark_theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, vjust = .25, hjust = 0))
+
+
+
+## ----writeManifest, echo=FALSE, eval=FALSE---------------------------------------------------------------------------
 ## installed.packages()
 ## # Oppdaterer manifest -- jeg tror vi må kjøre denne manuelt hver
 ## # gang før vi pusher til github. uio-serveren er hvertfall ikke fornøyd
