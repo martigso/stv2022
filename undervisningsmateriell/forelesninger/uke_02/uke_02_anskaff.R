a5118cea20fbe58a4018cd21691950ddd6ec3f71
Martin Søyland <martin.soyland@stv.uio.no>
2022-08-30 14:47:46 +0200

    la til innhold på forelesning nr.2

--- undervisningsmateriell/forelesninger/uke_02/uke_02_anskaff.R
+++ undervisningsmateriell/forelesninger/uke_02/uke_02_anskaff.R
@@ -1,4 +1,4 @@
-## ----setup, include=FALSE,warning=FALSE,message=FALSE-------------------------------------
+## ----setup, include=FALSE,warning=FALSE,message=FALSE------------------------------------------------------------------
 # setwd("./undervisningsmateriell/forelesninger/uke_02/")
 knitr::opts_chunk$set(echo = FALSE)
 knitr::opts_chunk$set(class.source = "code-bg")
@@ -9,7 +9,7 @@ library(tidyverse)
 library(tweetrmd)
 
 
-## ----struct_ex, echo=-1-------------------------------------------------------------------
+## ----struct_ex, echo=-1------------------------------------------------------------------------------------------------
 set.seed(984984)
 tibble(stortingscrape::cases$root) %>%      # Henter ut data fra stortingscrape
   select(id, status, type, title_short) %>% # Trekker ut relevante variabler
@@ -20,7 +20,7 @@ tibble(stortingscrape::cases$root) %>%      # Henter ut data fra stortingscrape
 ## xmllint --encode utf8 --format xml_eks.xml
 
 
-## ----tweet_regjeringen, out.width=".75\textwidth"-----------------------------------------
+## ----tweet_regjeringen, out.width=".75\textwidth"----------------------------------------------------------------------
 tweet_screenshot(
   tweet_url("Regjeringen", "1526207283872641025"),
   theme = "dark",
@@ -29,13 +29,13 @@ tweet_screenshot(
 )
 
 
-## ----nou_base, eval=-c(9,10), echo=TRUE---------------------------------------------------
+## ----nou_base, eval=-c(9,10), echo=TRUE--------------------------------------------------------------------------------
 
 library(rvest)
 library(stringr)
 
 # rot-url for regjeringen.no
-base_url <- "https://www.regjeringen.no/"
+base_url <- "https://www.regjeringen.no"
 
 # Laster ned siden med alle NOUer fra UD (atm 7 stk)
 str_c(base_url, "no/dokument/nou-ar/id1767/?ownerid=833") %>% 
@@ -43,13 +43,13 @@ str_c(base_url, "no/dokument/nou-ar/id1767/?ownerid=833") %>%
 
 # Trekker ut linkene til hver NOU
 nou_links <- read_html("./scrape/base.html") %>% 
-  html_elements("h2 > a[data-hitid]") %>% 
+  html_elements("li > h2 > a[data-hitid]") %>% 
   html_attr("href")
 
-nou_links
+paste0(base_url, nou_links)
 
 
-## ----nou_forside, eval=FALSE, echo=TRUE---------------------------------------------------
+## ----nou_forside, eval=FALSE, echo=TRUE--------------------------------------------------------------------------------
 ## 
 ## # Laster ned forsiden til hver NOU
 ## for(i in nou_links){
@@ -77,7 +77,7 @@ nou_links
 ## 
 
 
-## ----sjekke_at_filer_er_der, echo=TRUE, eval=TRUE-----------------------------------------
+## ----sjekke_at_filer_er_der, echo=TRUE, eval=TRUE----------------------------------------------------------------------
 
 # Lister opp forsidene vi har lagret
 list.files("./scrape/nou_forside/")
@@ -93,7 +93,7 @@ nou_forsider[[1]]
 
 
 
-## ----nou_pdf_links, echo=TRUE, eval=TRUE--------------------------------------------------
+## ----nou_pdf_links, echo=TRUE, eval=TRUE-------------------------------------------------------------------------------
 
 # Prosesserer hvert listeelement
 nou_pdf_links <- sapply(nou_forsider, function(x){
@@ -113,7 +113,7 @@ nou_pdf_links <- sapply(nou_forsider, function(x){
 nou_pdf_links
 
 
-## ----nou_pdf_dl, echo=TRUE, eval=FALSE----------------------------------------------------
+## ----nou_pdf_dl, echo=TRUE, eval=FALSE---------------------------------------------------------------------------------
 ## # Laster ned pdf til hver NOU
 ## for(i in nou_pdf_links){
 ## 
@@ -140,11 +140,11 @@ nou_pdf_links
 ## list.files("./scrape/nou_pdf/")
 
 
-## ----juksechunk, echo=FALSE, eval=TRUE----------------------------------------------------
+## ----juksechunk, echo=FALSE, eval=TRUE---------------------------------------------------------------------------------
 list.files("./scrape/nou_pdf/")
 
 
-## ----nou_lese_pdf, eval=TRUE, echo=TRUE, message=FALSE, warning=FALSE---------------------
+## ----nou_lese_pdf, eval=TRUE, echo=TRUE, message=FALSE, warning=FALSE--------------------------------------------------
 library(textreadr)
 
 # Lager objekt for filbane til alle pdfene
@@ -163,7 +163,7 @@ nou_tekst[[1]] %>%
   head(., 4)
 
 
-## ----nou_txt, eval=FALSE, echo=TRUE-------------------------------------------------------
+## ----nou_txt, eval=FALSE, echo=TRUE------------------------------------------------------------------------------------
 ## 
 ## # Fra 1 til 7 (antall NOUer) ...
 ## lapply(1:length(nou_tekst), function(x){
@@ -186,7 +186,7 @@ nou_tekst[[1]] %>%
 ## })
 
 
-## ----nou_analyse, echo=FALSE, error=FALSE, warning=FALSE, message=FALSE-------------------
+## ----nou_analyse, echo=FALSE, error=FALSE, warning=FALSE, message=FALSE------------------------------------------------
 library(tidytext)
 
 # Hentet fra: https://no.wikipedia.org/wiki/Liste_over_land_etter_areal
@@ -242,7 +242,7 @@ nou_data %>%
 
 
 
-## ----skand_reg, echo=FALSE, results='asis'------------------------------------------------
+## ----skand_reg, echo=FALSE, results='asis'-----------------------------------------------------------------------------
 (lm(n ~ skandinavia, data = nou_data)) %>%                   # Lager en regresjon med n som AV og skand. som UV
   stargazer::stargazer(.,                                    # Lager tabell av regresjonen
                        type = "html",                        # som skal komme ut i .html
@@ -251,7 +251,7 @@ nou_data %>%
                                             "Konstantledd"))
 
 
-## ----en_kravler, eval=FALSE, echo=TRUE----------------------------------------------------
+## ----en_kravler, eval=FALSE, echo=TRUE---------------------------------------------------------------------------------
 ## 
 ## # Laster inn pakke for kravling
 ## library(Rcrawler)
@@ -266,7 +266,7 @@ nou_data %>%
 ## 
 
 
-## ----juksechunk2, eval=TRUE, echo=TRUE----------------------------------------------------
+## ----juksechunk2, eval=TRUE, echo=TRUE---------------------------------------------------------------------------------
 # Lager en vektor med alle filnavn
 virkord_filer <- list.files("./crawl/virksommeord.no-101413", 
                             full.names = TRUE)
@@ -279,7 +279,7 @@ length(virkord_filer)
 
 
 
-## ---- eval=FALSE, echo = TRUE-------------------------------------------------------------
+## ---- eval=FALSE, echo = TRUE------------------------------------------------------------------------------------------
 ## 
 ## # Leser inn alle filene fra kravlingen
 ## virkord_html <- lapply(virkord_filer, rvest::read_html)
@@ -344,16 +344,16 @@ length(virkord_filer)
 ## # save(virkord, file = "./crawl/virksommeord.rda")
 
 
-## ----load_virkdata, echo = FALSE, eval=TRUE-----------------------------------------------
+## ----load_virkdata, echo = FALSE, eval=TRUE----------------------------------------------------------------------------
 load("./crawl/virksommeord.rda")
 
 
-## ----vise_virkdata, echo=TRUE, eval=TRUE--------------------------------------------------
+## ----vise_virkdata, echo=TRUE, eval=TRUE-------------------------------------------------------------------------------
 # Viser de 6 øverste radene i datasettet
 virkord %>% sample_n(6)
 
 
-## ----robots, eval=-1----------------------------------------------------------------------
+## ----robots, eval=-1---------------------------------------------------------------------------------------------------
 download.file("https://vg.no/robots.txt", destfile = "./scrape/vg_robots.txt")
 
 readLines("./scrape/vg_robots.txt")
@@ -422,15 +422,15 @@ readLines("./scrape/vg_robots.txt")
 ## head -n 10 tekst.txt
 
 
-## ----show_ocr_text, echo = FALSE, eval = TRUE, warning = FALSE----------------------------
+## ----show_ocr_text, echo = FALSE, eval = TRUE, warning = FALSE---------------------------------------------------------
 readLines("./tekst.txt", n = 7)
 
 
-## ----show_noisy_ocr_text, echo = FALSE, eval = TRUE, warning = FALSE----------------------
+## ----show_noisy_ocr_text, echo = FALSE, eval = TRUE, warning = FALSE---------------------------------------------------
 readLines("./tekst_noisy.txt", n = 5)
 
 
-## ----bow_ex, echo=FALSE, eval=TRUE, warning=FALSE, message=FALSE--------------------------
+## ----bow_ex, echo=FALSE, eval=TRUE, warning=FALSE, message=FALSE-------------------------------------------------------
 
 samp_tekst <- read_csv("st_debates_meta.csv", show_col_types = FALSE)
 
@@ -439,7 +439,7 @@ set.seed(4093)
 samp_tekst[sample(1:nrow(samp_tekst), 6), c("speech_id", "name_fixed", "speech")]
 
 
-## ----bow_ex2, echo=FALSE, eval=TRUE, warning=FALSE, message=FALSE-------------------------
+## ----bow_ex2, echo=FALSE, eval=TRUE, warning=FALSE, message=FALSE------------------------------------------------------
 samp_tekst <- samp_tekst[sample(1:nrow(samp_tekst), 100), ]
 
 samp_tekst <- samp_tekst %>%
@@ -450,4 +450,8 @@ samp_tekst <- samp_tekst %>%
 
 as.matrix(samp_tekst[1:6, c("det", "har", "jord", "lærere", "behandling", "park")])
 
+
+
+## ----writeScript, echo=FALSE, eval=FALSE-------------------------------------------------------------------------------
+## knitr::purl("./uke_02_anskaff.Rmd", "./uke_02_anskaff.R", documentation = 1)
 
